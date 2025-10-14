533df41a94b6d86d8f44fd3b9e797bbc250bc486
reshika <reshikakm@gmail.com>
2025-10-14 18:27:46 +0530

    fix: login issues

--- backend/server.js
+++ backend/server.js
@@ -6,20 +6,16 @@ const authRoutes = require('./routes/auth');
 dotenv.config();
 const app = express();
 
-app.listen(process.env.PORT, () => {
-  console.log("server running")
-})
-
 // Middleware
-// app.use(express.json());
+app.use(express.json());
 
 // Routes
-// app.use('/api/auth', authRoutes);
+app.use('/api/auth', authRoutes);
 
-// // Connect to MongoDB and start server
-// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => {
-//     console.log('Connected to MongoDB');
-//     app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
-//   })
-//   .catch(err => console.log(err));
+// Connect to MongoDB and start server
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+  })
+  .catch(err => console.log(err));
